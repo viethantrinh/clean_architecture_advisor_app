7de405ad86caef7c88cbc021aa3d1b09ec0cd2c9
Viet Han Trinh <hntrnn12@gmail.com>
2025-01-23 09:45:50 +0700

    set up theme swtich in project with provider

---
+++ lib/application/pages/advice/advice_page.dart
@@ -0,0 +1,32 @@
+import 'package:clean_architecture_advisor_app/application/core/services/theme_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AdvicerPage extends StatelessWidget {
+  const AdvicerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Advicer',
+          style: themeData.textTheme.displayLarge,
+        ),
+        centerTitle: true,
+        actions: [
+          Switch(
+            value: Provider
+                .of<ThemeService>(context)
+                .isDarkModeOn,
+            onChanged: (_) {
+              Provider.of<ThemeService>(context, listen: false).toggleTheme();
+            },
+          ),
+        ],
+      ),
+      body: const Placeholder(),
+    );
+  }
+}
